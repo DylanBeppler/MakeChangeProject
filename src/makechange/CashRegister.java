5f0af9a0246aa2244f2fbdcb65a9df7a3fb8914f
DylanBeppler <dylanbeppler@gmail.com>
2023-11-19 18:07:49 -0600

    completed makechange project

--- src/makechange/CashRegister.java
+++ src/makechange/CashRegister.java
@@ -5,20 +5,20 @@ import java.util.Scanner;
 public class CashRegister {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int twentyd = 20;
-		int tend = 10;
-		int fived = 5;
-		int oned = 1;
+		int twentyd = 2000;
+		int tend = 1000;
+		int fived = 500;
+		int oned = 100;
 
-		double quarter = 0.25;
-		double dime = 0.10;
-		double nickel = 0.05;
-		double penny = 0.01;
+		double quarter = 25;
+		double dime = 10;
+		double nickel = 5;
+		double penny = 1;
 
 		System.out.println("What is the cost of the item being purchased? ");
-		double cost = sc.nextDouble();
+		double cost = sc.nextDouble() * 100;
 		System.out.println("How much money was received? ");
-		double received = sc.nextDouble();
+		double received = sc.nextDouble() * 100;
 		double change = received - cost;
 
 		if (received == cost) {
@@ -27,9 +27,9 @@ public class CashRegister {
 			System.out.println("You have received too little money.");
 		} else {
 			change = Math.round(change * 100.0) / 100.0;
-			System.out.println("The customer's change is " + change);
+			System.out.println("The customer's change is " + change / 100);
 			System.out.print("The customer will receive: ");
-		
+
 			int numTwenties = (int) (change / twentyd);
 			change %= twentyd;
 
@@ -52,38 +52,38 @@ public class CashRegister {
 			change %= nickel;
 
 			int numPennies = (int) (change / penny);
+			change = (int) ((change % penny) * 100) / 100.0;
 
-		
 			if (numTwenties > 0) {
-				System.out.print(numTwenties + " twenty dollar bills ");
+				System.out.print(numTwenties + " twenty dollar bills. ");
 			}
 
 			if (numTens > 0) {
-				System.out.print(numTens + " ten dollar bills ");
+				System.out.print(numTens + " ten dollar bills. ");
 			}
 
 			if (numFives > 0) {
-				System.out.print(numFives + " five dollar bills ");
+				System.out.print(numFives + " five dollar bills. ");
 			}
 
 			if (numOnes > 0) {
-				System.out.print(numOnes + " one dollar bills ");
+				System.out.print(numOnes + " one dollar bills. ");
 			}
 
 			if (numQuarters > 0) {
-				System.out.print(numQuarters + " quarters ");
+				System.out.print(numQuarters + " quarters. ");
 			}
 
 			if (numDimes > 0) {
-				System.out.print(numDimes + " dimes ");
+				System.out.print(numDimes + " dimes. ");
 			}
 
 			if (numNickels > 0) {
-				System.out.print(numNickels + " nickels ");
+				System.out.print(numNickels + " nickels. ");
 			}
 
 			if (numPennies > 0) {
-				System.out.print(numPennies + " pennies ");
+				System.out.print(numPennies + " pennies. ");
 			}
 		}
 
@@ -93,3 +93,5 @@ public class CashRegister {
 }
 // mod operator super usefull, looping not good for this //can be done entirely
 // in main method //leave out 0's //division operator important
+
+
